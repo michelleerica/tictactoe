768e459a6be9f2c5167f600ed18345d35ebaeef5
Michelle Erica <michelle.erica.lo@gmail.com>
2017-05-29 17:36:12 +1000

    Monday afternoon

--- js/main.js
+++ js/main.js
@@ -4,15 +4,22 @@ console.log('test')
 var oImage = 'http://static.wixstatic.com/media/c09ee4_6b9a876a03844e25a314e5ed36d649a3.png';
 var xImage = 'http://vignette2.wikia.nocookie.net/animal-jam-clans-1/images/2/2e/Transparent_City_Sticker.png/revision/latest?cb=20170111222644'
 
+$(document).ready(function () {
+
+var divTracker = $('.cell').bind('click', function(){
+    var tracker = $(this).attr('id');
+    console.log(tracker + "from tracker")
+    return tracker;// gets the id of a clicked link that has a class of menu. THIS NUMBER IS NOT PULLING THROUGH TO game.
+});
 
 var gameLogic = {
 
-  trackPlayerOne: function (div) {
-    gameLogic.game[div] = 1;
+  trackPlayerOne: function (y) {
+    gameLogic.game[y] = 1;
   },
 
-  trackPlayerTwo: function (div) {
-    gameLogic.game[div] = 2;
+  trackPlayerTwo: function (x) {
+    gameLogic.game[x] = 2;
   },
 
   checkWin: function() {
@@ -26,21 +33,29 @@ var gameLogic = {
     if (hits % 2 !== 0) { //for hits 2,4,6,8 etc
       var $imgX = $('<img>').attr('src', xImage);
       $(this).append( $imgX );
-      console.log('is this working?');
-      x = // need to pull through the DIV ID
+      var x = divTracker;// need to pull through the DIV ID
+      console.log(x + "checking line 35");
       gameLogic.trackPlayerTwo(x);
+      console.log(gameLogic.game);
     } else { // for hits 1,3,5,7
         var $imgO = $('<img>').attr('src', oImage);
         $(this).append( $imgO);
-        console.log('else')
-        y = // need to pull through the DIV ID
-        gameLogic.trackPlayerOne(y); 
+        var y = divTracker;// need to pull through the DIV ID from divTracker above
+        console.log(y + "checking 42");
+        gameLogic.trackPlayerOne(y);
+        console.log(gameLogic.game + "line 43");
+
     }
      hits++;
      return false;
       });
   })
+
 }
+
+
+}); //end of .ready
+
 //to access img:
 // var $ax = $('#ax');
 // $ax.html()
