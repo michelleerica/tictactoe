31ac13a824b18b1a71c160083024fe8f58c1dfbc
Michelle Erica <michelle.erica.lo@gmail.com>
2017-06-02 12:44:43 +1000

    removed working drafts and identing updated

--- js/main.js
+++ js/main.js
@@ -10,115 +10,184 @@ var players = {
     score: 0,
     image: 'img/heartbleed.png',
     scoreID: "#p2score"
+  },
+  billMurray: {
+    number: 'billMurray',
+    score: 0,
+    image: 'http://www.fillmurray.com/125/125',
+    scoreID: "#p3score"
   }
 };
+
 var hits = 0;
 var draw = 0;
-var currentPlayer = 'x';
+var currentPlayer = "x";
+var billMode = false;
+var endGame = false;
 
 $(document).ready(function () {
+//click box for image to appear and checks to run
   $('.cell').on("click", function(){
-    var cellID = this.id;
-
-    if( gameLogic.game[this.id] !== 0 || gameLogic.game === 'gameover' ){
+    var cellId = this.id;
+    var bCellId = parseInt((Math.random() * 8));
+    if ( (gameLogic.game[this.id] !== 0) || (endGame === true) ){
       return;
     }
     var gamePlay = function(player, cellSelected){
+      endGame = false;
       var $img = $('<img>').attr('src', players[player].image);
       $("#"+ cellSelected).append( $img );
       $('img').addClass("inPlay");
       gameLogic.updateBoardArray(player, cellSelected);
-      if (player === 'x')
-        {gameLogic.winDetector('x');
-        currentPlayer = 'o';
-      } else if (player === 'o') {
+
+
+      if (player === 'x'){
+        gameLogic.winDetector('x');
+          if (billMode === false){
+          currentPlayer = 'o';
+          }
+      }
+        else if (player === 'o') {
         gameLogic.winDetector('o');
-        currentPlayer ='x'
+        currentPlayer ='x';
+      }
+
+
+      if (billMode === false || endGame===true){
+        return
+      }
+      else if (gameLogic.game[bCellId] === 0){
+        var $bimg = $('<img>').attr('src', players.billMurray.image);
+        $("#"+ bCellId).append( $bimg );
+        $('img').addClass("inPlay");
+        gameLogic.updateBoardArray('billMurray', bCellId);
+        console.log(bCellId + "game:" + gameLogic.game);
+        gameLogic.winDetector('billMurray');
+      } else {
+        bCellId = gameLogic.game.indexOf(0);
+        var $bimg = $('<img>').attr('src', players.billMurray.image);
+        $("#"+ bCellId).append( $bimg );
+        $('img').addClass("inPlay");
+        gameLogic.updateBoardArray('billMurray', bCellId);
+        gameLogic.winDetector('billMurray');
       }
     }
-    gamePlay(currentPlayer, cellID);
+    gamePlay(currentPlayer, cellId);
     hits++;
-});
+  });
 
-  //reset game board
-  $( ".resetButton" ).on("click", function() {
-    gameLogic.game = [0,0,0,0,0,0,0,0,0],
+  var gameLogic = {
+    game: [0,0,0,0,0,0,0,0,0], // array depicting gameboard
+
+    win:[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]], //  array of possible wins
+
+    //function to update gameboard array
+    updateBoardArray: function( currentPlayer, z ) {
+      if (players[ currentPlayer ].number  === 1){
+        gameLogic.game[z] = "x";
+      }
+      else if (players[ currentPlayer ].number  === 2){
+        gameLogic.game[z] = "o";
+      } else {
+        gameLogic.game[z] = "billMurray"
+      };
+    },
+
+    //function to detect if a player has won
+    winDetector: function (player) {
+      for (var i = 0; i < gameLogic.win.length; i++) {
+        var possWins = gameLogic.win[i];
+        var a = possWins[0];
+        var b = possWins[1];
+        var c = possWins[2];
+
+        var aVal = gameLogic.game[a];
+        var bVal = gameLogic.game[b];
+        var cVal = gameLogic.game[c];
+        win = (aVal === player && bVal === player && cVal === player);
+
+        if (win){
+          gameLogic.winNotification(player);
+          return player;
+        }
+      } // for
+
+      if ( (billMode === false) && !win && hits >= 8 ){
+          gameLogic.drawNotification();
+          return false;
+      }
+      else if ((billMode === true) && !win && hits >= 4 ){
+        gameLogic.drawNotification();
+        return false;
+      }
+    },
+
+    //function to alert of win
+    winNotification: function(winner) {
+      players[ winner ].score++;
+      $('.cell').css('backgroundColor', '#FDF0D5');
+      $('.grid').addClass('animated bounce flash');
+      $("body").css('backgroundColor', '#D81E5B');
+      $("h1").addClass('notification animated bounce swing rollIn');
+      $(".scoreboard").addClass('animated swing');
+      $(players[ winner ].scoreID).html(players[ winner ].score);
+      if (players[ winner ].number === "billMurray"){
+        $("h1").html("Bill Murray won!");
+      } else {
+        $("h1").html("Player " +  players[ winner ].number  + " won!");
+      }
+      gameLogic.gameOver()
+      return;
+    },
+    //function to alert there is a draw
+    drawNotification: function() {
+      draw++;
+      $('#draw').html(draw);
+      $('.cell').css('backgroundColor', 'grey');
+      $('.grid').addClass('animated bounce flash');
+      $("body").css('backgroundColor', 'rgba(121, 119, 122, 0.42)');
+      $("h1").html("It's a draw").addClass('notification animated swing rollIn');
+      gameLogic.gameOver()
+      return;
+    },
+    //function to reflect game is over
+    gameOver: function () {
+      endGame = true;
+      currentPlayer = 'x';
+      return
+    }
+  } //close gameLogic object
+
+
+  var reset = function(){
+    gameLogic.game = [0,0,0,0,0,0,0,0,0];
     $('.inPlay').remove();
-    $('.cell')
-      .on("click")
-      .css('backgroundColor', '#7586B7');
+    $('.cell').css('backgroundColor', '#7586B7');
     $('.animated bounce flash').remove();
     $('.scoreboard').addClass('bounceIn');
     $("body").css('backgroundColor', '#A7CAB1');
     $("h1").html("Play again");
     $('.animated bounce swing rollIn').remove();
     hits = 0;
-  })
+    endGame = false;
+  };
 
-var gameLogic = {
-  game: [0,0,0,0,0,0,0,0,0], // array depicting gameboard
+  $( ".resetButton" ).on("click", reset );
 
-  win:[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]], //  array of possible wins
+  $( ".twoPlayerButton" ).on("click", function() {
+    reset();
+    currentPlayer='o';
+    billMode = false;
+    hits = 0;
+  });
 
-  //function to update gameboard array
-  updateBoardArray: function( currentPlayer, z ) {
-    if (players[ currentPlayer ].number  === 1){
-      gameLogic.game[z] = "x";
-    } else { gameLogic.game[z] = "o"}
-  },
-
-  //function to detect if a player has won
-  winDetector: function (player) {
-    var win = false;
-    for (var i = 0; i < gameLogic.win.length; i++) {
-      var possWins = gameLogic.win[i];
-      var a = possWins[0];
-      var b = possWins[1];
-      var c = possWins[2];
-
-      var aVal = gameLogic.game[a];
-      var bVal = gameLogic.game[b];
-      var cVal = gameLogic.game[c];
-      win = (aVal === player && bVal === player && cVal === player);
-      if (win){
-        gameLogic.winNotification(player);
-        return player;
-      }
-    } // for
-
-    if ( !win && hits >= 8 ){
-        gameLogic.drawNotification();
-        return false;
-    }
-  },
-
-  //function to alert of win
-  winNotification: function(winner) {
-    $('.cell')
-      .css('backgroundColor', '#FDF0D5')
-    $('.grid').addClass('animated bounce flash');
-    $("body").css('backgroundColor', '#D81E5B');
-    $("h1").html("Player " +  players[ winner ].number  + " won!").addClass('animated bounce swing rollIn');
-    $(".scoreboard").addClass('animated swing');
-    players[ winner ].score++;
-    $(players[ winner ].scoreID).html(players[ winner ].score)
-    gameLogic.gameOver()
-  },
-  //function to alert there is a draw
-  drawNotification: function() {
-    draw++;
-    $('#draw').html(draw);
-    $('.cell')
-      .css('backgroundColor', 'grey')
-    $('.grid').addClass('animated bounce flash');
-    $("body").css('backgroundColor', 'rgba(121, 119, 122, 0.42)');
-    $("h1").html("It's a draw").addClass('animated bounce swing rollIn');
-    gameLogic.gameOver()
-  },
-  //function to reflect game is over
-  gameOver: function () {
-    gameLogic.game = 'gameover';
+    //bill mode
+  $( ".playBillButton" ).on("click", function() {
+    reset()
     currentPlayer = 'x';
-  }
-} //close object
+    billMode = true;
+    hits = 0;
+    $("h1").html("You vs Bill");
+  });
+
 }); //end of .ready
